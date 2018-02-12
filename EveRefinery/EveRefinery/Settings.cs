71c5fe46afaed6c2fc77ccf21cbc909c735f59a5
codeguard <alexandr.miloslavskiy@gmail.com>
2018-02-12 22:35:17 +0300

    Code cleanup: Settings format 0 is no longer supported

--- EveRefinery/EveRefinery/Settings.cs
+++ EveRefinery/EveRefinery/Settings.cs
@@ -177,147 +177,6 @@ namespace EveRefinery
             return version;
         }
 
-        public class V0
-        {
-            private static void DoPriceSettings(ref PriceSettings a_Result, XmlNode a_Xml, XmlNamespaceManager a_Namespace)
-            {
-                if (null == a_Xml)
-                    return;
-
-                a_Result.Provider = PriceProviders.EveCentral;
-                TryImportSetting(ref a_Result.RegionID,     a_Xml.SelectSingleNode("x:RegionID", a_Namespace));
-                TryImportSetting(ref a_Result.SolarID,      a_Xml.SelectSingleNode("x:SolarID", a_Namespace));
-                TryImportSetting(ref a_Result.StationID,    a_Xml.SelectSingleNode("x:StationID", a_Namespace));
-                TryImportSetting(ref a_Result.PriceType,    a_Xml.SelectSingleNode("x:PriceType", a_Namespace));
-            }
-
-            private static void DoOptions(Settings a_Settings, XmlNode a_Xml, XmlNamespaceManager a_Namespace)
-            {
-                if (null == a_Xml)
-                    return;
-
-                TryImportSetting(ref a_Settings.Options.DBPath,                 a_Xml.SelectSingleNode("x:DBPath", a_Namespace));
-                TryImportSetting(ref a_Settings.Options.CheckUpdates,           a_Xml.SelectSingleNode("x:CheckUpdates", a_Namespace));
-
-                DoPriceSettings(ref a_Settings.PriceLoad.SourceMinerals,        a_Xml.SelectSingleNode("x:PriceSettings_Minerals", a_Namespace), a_Namespace);
-                DoPriceSettings(ref a_Settings.PriceLoad.SourceItems,           a_Xml.SelectSingleNode("x:PriceSettings_Items", a_Namespace), a_Namespace);
-                TryImportSetting(ref a_Settings.PriceLoad.ItemsHistoryDays,     a_Xml.SelectSingleNode("x:PriceHistoryDays", a_Namespace));
-                TryImportSetting(ref a_Settings.PriceLoad.ItemsExpiryDays,      a_Xml.SelectSingleNode("x:PriceExpiryDays", a_Namespace));
-                TryImportSetting(ref a_Settings.PriceLoad.MineralExpiryDays,    a_Xml.SelectSingleNode("x:MineralPriceExpiryDays", a_Namespace));
-
-                TryImportSetting(ref a_Settings.Appearance.RedPrice,              a_Xml.SelectSingleNode("x:RedPrice", a_Namespace));
-                TryImportSetting(ref a_Settings.Appearance.GreenPrice,            a_Xml.SelectSingleNode("x:GreenPrice", a_Namespace));
-                TryImportSetting(ref a_Settings.Appearance.UseAssetQuantities,    a_Xml.SelectSingleNode("x:UseAssetQuantities", a_Namespace));
-                TryImportSetting(ref a_Settings.Appearance.OverrideAssetsColors,  a_Xml.SelectSingleNode("x:OverrideAssetsColors", a_Namespace));
-                TryImportSetting(ref a_Settings.Appearance.RedIskLoss,            a_Xml.SelectSingleNode("x:RedIskLoss", a_Namespace));
-                TryImportSetting(ref a_Settings.Appearance.GreenIskLoss,          a_Xml.SelectSingleNode("x:GreenIskLoss", a_Namespace));
-            }
-
-            private static void DoApiCharacters(Settings a_Settings, XmlNodeList a_Xml, XmlNamespaceManager a_Namespace)
-            {
-                if (null == a_Xml)
-                    return;
-
-                foreach (XmlNode currNode in a_Xml)
-                {
-                    Settings._ApiAccess.Char currItem = new Settings._ApiAccess.Char();
-                    TryImportSetting(ref currItem.KeyID,            currNode.SelectSingleNode("x:KeyID", a_Namespace));
-                    TryImportSetting(ref currItem.CharacterID,      currNode.SelectSingleNode("x:CharacterID", a_Namespace));
-                    TryImportSetting(ref currItem.CharacterName,    currNode.SelectSingleNode("x:CharacterName", a_Namespace));
-                    TryImportSetting(ref currItem.CorporationName,  currNode.SelectSingleNode("x:CorporationName", a_Namespace));
-                    a_Settings.ApiAccess.Chars.Add(currItem);
-                }
-            }
-
-            private static void DoApiKeys(Settings a_Settings, XmlNodeList a_Xml, XmlNamespaceManager a_Namespace)
-            {
-                if (null == a_Xml)
-                    return;
-
-                foreach (XmlNode currNode in a_Xml)
-                {
-                    Settings._ApiAccess.Key currItem = new Settings._ApiAccess.Key();
-                    TryImportSetting(ref currItem.KeyID,            currNode.SelectSingleNode("x:KeyID", a_Namespace));
-                    TryImportSetting(ref currItem.Verification,     currNode.SelectSingleNode("x:Verification", a_Namespace));
-                    a_Settings.ApiAccess.Keys.Add(currItem);
-                }
-            }
-
-            private static void DoFormLocation(Settings a_Settings, XmlNode a_Xml, XmlNamespaceManager a_Namespace)
-            {
-                if (null == a_Xml)
-                    return;
-
-                TryImportSetting(ref a_Settings.UILocations.MainWindow.X0,      a_Xml.SelectSingleNode("x:FormLocation/x:X", a_Namespace));
-                TryImportSetting(ref a_Settings.UILocations.MainWindow.Y0,      a_Xml.SelectSingleNode("x:FormLocation/x:Y", a_Namespace));
-                TryImportSetting(ref a_Settings.UILocations.MainWindow.CX,      a_Xml.SelectSingleNode("x:FormSize/x:Width", a_Namespace));
-                TryImportSetting(ref a_Settings.UILocations.MainWindow.CY,      a_Xml.SelectSingleNode("x:FormSize/x:Height", a_Namespace));
-            }
-
-            private static void DoViewColumns(Settings a_Settings, XmlNodeList a_Xml, XmlNamespaceManager a_Namespace)
-            {
-                if (null == a_Xml)
-                    return;
-
-                foreach (XmlNode currNode in a_Xml)
-                {
-                    Settings._UILocations.Column currItem = new Settings._UILocations.Column();
-                    TryImportSetting(ref currItem.Name,             currNode.SelectSingleNode("x:Name", a_Namespace));
-                    TryImportSetting(ref currItem.Index,            currNode.SelectSingleNode("x:Index", a_Namespace));
-                    TryImportSetting(ref currItem.Visible,          currNode.SelectSingleNode("x:Visible", a_Namespace));
-                    TryImportSetting(ref currItem.Width,            currNode.SelectSingleNode("x:Width", a_Namespace));
-                    a_Settings.UILocations.Columns.Add(currItem);
-                }
-            }
-
-            private static void DoToolbars(Settings a_Settings, XmlNodeList a_Xml, XmlNamespaceManager a_Namespace)
-            {
-                if (null == a_Xml)
-                    return;
-
-                foreach (XmlNode currNode in a_Xml)
-                {
-                    Settings._UILocations.Toolbar currItem = new Settings._UILocations.Toolbar();
-                    TryImportSetting(ref currItem.Name,             currNode.SelectSingleNode("x:Name", a_Namespace));
-                    TryImportSetting(ref currItem.Location.X0,      currNode.SelectSingleNode("x:Location/x:X", a_Namespace));
-                    TryImportSetting(ref currItem.Location.Y0,      currNode.SelectSingleNode("x:Location/x:Y", a_Namespace));
-                    TryImportSetting(ref currItem.Location.CX,      currNode.SelectSingleNode("x:Size/x:Width", a_Namespace));
-                    TryImportSetting(ref currItem.Location.CY,      currNode.SelectSingleNode("x:Size/x:Height", a_Namespace));
-                    TryImportSetting(ref currItem.Panel,            currNode.SelectSingleNode("x:Panel", a_Namespace));
-                    a_Settings.UILocations.Toolbars.Add(currItem);
-                }
-            }
-
-            private static void DoSettings(Settings a_Settings, XmlNode a_Xml, XmlNamespaceManager a_Namespace)
-            {
-                if (null == a_Xml)
-                    return;
-
-                DoOptions(a_Settings,       a_Xml.SelectSingleNode("x:Options", a_Namespace), a_Namespace);
-                DoApiCharacters(a_Settings, a_Xml.SelectNodes("x:ApiCharacters", a_Namespace), a_Namespace);
-                DoApiKeys(a_Settings,       a_Xml.SelectNodes("x:ApiKeys", a_Namespace), a_Namespace);
-                DoFormLocation(a_Settings,  a_Xml.SelectSingleNode("x:Locations", a_Namespace), a_Namespace);
-                DoViewColumns(a_Settings,   a_Xml.SelectNodes("x:ViewColumns", a_Namespace), a_Namespace);
-                DoToolbars(a_Settings,      a_Xml.SelectNodes("x:Toolbars", a_Namespace), a_Namespace);
-            }
-
-            public static Settings Load(String a_Path)
-            {
-                Settings result = new Settings();
-
-                XmlDocument xmlDocument = new XmlDocument();
-                xmlDocument.Load(a_Path);
-
-                XmlNamespaceManager xmlNamespace = new XmlNamespaceManager(xmlDocument.NameTable);
-                xmlNamespace.AddNamespace("x", "http://tempuri.org/Settings.xsd");
-
-                XmlNode xmlSettingsNode = xmlDocument.SelectSingleNode("/x:Settings", xmlNamespace);
-                DoSettings(result, xmlSettingsNode, xmlNamespace);
-
-                return result;
-            }
-        }
-
         public class V1
         {
             public static Settings Load(String a_Path)
@@ -355,10 +214,9 @@ namespace EveRefinery
                 UInt32 version = DetectSettingsVersion(settingsPath);
                 switch (version)
                 {
-                case 0:
-                    return V0.Load(settingsPath);
                 case 1:
                     return V1.Load(settingsPath);
+				case 0:
                 default:
                     return new Settings();
                 }
